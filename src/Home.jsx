55c9f0326e3706697d5ebb055ec5d74e6e115d3a
Aryan Neupane <official.aryanneupane@gmail.com>
2024-09-03 20:34:56 +0545

    Inital Commit

---
+++ src/Home.jsx
@@ -0,0 +1,32 @@
+import './Home.css'
+import{Link} from 'react-router-dom'
+function Home() {
+    return (
+        <>
+            <nav>
+                <div className="container">
+                    <div className="logo">MyWebsite</div>
+                    <ul className="nav-links">
+                        <li><a href="#">Home</a></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>
+
+            <main>
+                <div className="container">
+                    <h1>Welcome to My Website</h1>
+                    <p>This is a simple webpage with a navigation bar at the top. You can add more content here as needed.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In efficitur, libero nec vehicula posuere, justo nulla consequat sapien, non cursus justo est sed mauris.</p>
+                </div>
+            </main>
+        </>
+    );
+
+
+    
+}
+
+export default Home
